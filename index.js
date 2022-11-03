c5b6546f8beba12e65c54c070ad31414ed6d0de6
Hamna7577 <88288760+Hamna7577@users.noreply.github.com>
2022-11-04 01:26:35 +0500

    update js file
    added "if statement" to stop counting when it hit a specific count

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 let tbcount = document.getElementById("tb-count")
 let entries = document.getElementById("entries")
 let count = 0;
+let value = 0;
 function increment() {
 
     count++;
     tbcount.textContent = count;
 }
 function save() {
+    value +=1;
     countStr = count + " - "
     entries.textContent += countStr;
     tbcount.textContent = 0;
     count = 0;
+    if (value > 10){
+        entries.textContent = "You've entered 10 entries reload page to start over!";
+    }
+    
 }
